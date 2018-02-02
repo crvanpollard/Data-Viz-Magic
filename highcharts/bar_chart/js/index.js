db391828abe9dce59f28bd4746e190cbf4f2bb79
Christopher Pollard <crvanpollard@gmail.com>
2018-02-01 21:33:37 -0500

    file upload
    modified size

--- highcharts/bar_chart/js/index.js
+++ highcharts/bar_chart/js/index.js
@@ -10,7 +10,7 @@ $(function () {
         yAxis: {
             allowDecimals: false,
             title: {
-                text: ''
+                text: 'Composite Value by Census Tract'
             },
             tickInterval: 10, 
             min: 0,
@@ -32,14 +32,12 @@ $(function () {
         plotOptions: {
             bar: {
                 colorByPoint: true
-            }
+            },
+             series: {
+            pointWidth: 15
+        }
         },
-        tooltip: {
-            formatter: function () {
-            return 'The value for <b>' + this.x +
-                '</b> is <b>' + this.y + '</b>';
-            }
-         },
+        tooltip: { enabled: false },
         legend: {
             enabled: false
         },
@@ -56,26 +54,45 @@ $(function () {
         chart.series[0].setData([45] );
         chart.yAxis[0].setExtremes(0,50);
         chart.update({
+            tooltip: { enabled: false },
             yAxis: {
+             title: {
+                text: 'Composite Value by Census Tract'
+            },    
             tickInterval: 10 
             }
         });
     });
     $('#youth').click(function() {
         chart.series[0].setData([{y:93,color:'#ffffcc'},   {y:263,color:'#a1dab4'},   {y:640,color:'#41b6c4'},   {y:294,color:'#2c7fb8'},   {y:79,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
+            tooltip: {
+                enabled: true,
+            formatter: function () {
+            return 'There are <b>' + this.y +
+                '</b> census tracts in this score</b>';
+            }
+            },
+            xAxis: {
+            title: {
+                text: 'Scores'
+            }
+            },   
             yAxis: {
+            title: {
+                text: 'Number of Census Tracts'
+            },    
             tickInterval: 100
         //    min: 0,
-        //    max: 1369
+        //    max: 900
             }
         });
     });
 
      $('#older').click(function() {
         chart.series[0].setData([{y:37,color:'#ffffcc'},   {y:371,color:'#a1dab4'},   {y:673,color:'#41b6c4'},   {y:200,color:'#2c7fb8'},   {y:88,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -84,7 +101,7 @@ $(function () {
     });
      $('#female').click(function() {
         chart.series[0].setData([{y:29,color:'#ffffcc'},   {y:304,color:'#a1dab4'},   {y:703,color:'#41b6c4'},   {y:277,color:'#2c7fb8'},  {y:56,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -93,7 +110,7 @@ $(function () {
     });
      $('#race').click(function() {
         chart.series[0].setData([{y:2,color:'#ffffcc'},    {y:612,color:'#a1dab4'},   {y:402,color:'#41b6c4'},   {y:152,color:'#2c7fb8'},  {y:201,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -102,7 +119,7 @@ $(function () {
     });
      $('#ethnic').click(function() {
         chart.series[0].setData([{y:28,color:'#ffffcc'},   {y:323,color:'#a1dab4'},   {y:827,color:'#41b6c4'},   {y:99,color:'#2c7fb8'},   {y:92,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -111,7 +128,7 @@ $(function () {
     });
      $('#foreign').click(function() {
         chart.series[0].setData([{y:4,color:'#ffffcc'},    {y:513,color:'#a1dab4'},   {y:553,color:'#41b6c4'},   {y:181,color:'#2c7fb8'},  {y:118,color:'#253494'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -120,7 +137,7 @@ $(function () {
     });
      $('#lep').click(function() {
         chart.series[0].setData([{y:24,color:'#ffffcc'},   {y:477,color:'#a1dab4'},  {y:629,color:'#41b6c4'},  {y:114,color:'#2c7fb8'},  {y:125,color:'#253594'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -129,7 +146,7 @@ $(function () {
     });
      $('#disabled').click(function() {
         chart.series[0].setData([{y:25,color:'#ffffcc'},   {y:424,color:'#a1dab4'},  {y:574,color:'#41b6c4'},  {y:251,color:'#2c7fb8'},  {y:95,color:'#253594'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
@@ -138,7 +155,7 @@ $(function () {
     });
        $('#income').click(function() {
         chart.series[0].setData([{y:3,color:'#ffffcc'},    {y:563,color:'#a1dab4'},  {y:444,color:'#41b6c4'},  {y:199,color:'#2c7fb8'},  {y:160,color:'#253594'}] );
-        chart.yAxis[0].setExtremes(0,1369);
+        chart.yAxis[0].setExtremes(0,900);
         chart.update({
             yAxis: {
             tickInterval: 100
